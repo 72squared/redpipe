5147fcfd4bd16207a20174a6a08f5877b9be8037
John Loehrer <john@happybits.co>
2017-05-07 10:21:55 -0700

    using pytest-benchmark tool for benchmark comparisons

--- bench.py
+++ bench.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 """
-Really crude and unscientific benchmarking tool.
-
-Hope to make it better soon.
-
 Compares redis, pipeline, and redpipe calls.
 
 Over localhost socket.
@@ -18,61 +14,81 @@ But it is much easier to work with.
 This doesn't compare actual impact of using futures and variable manipulation.
 But I am going to bet that for most environments, the impact of network will
 far far outweigh any other factor.
+
+Usage
+-----
+
+run with `py.test`:
+
+.. code-block:: bash
+
+    py.test ./bench.py
+
+Experimenting with pytest.benchmark plugin:
+
+http://pytest-benchmark.readthedocs.io/
+
+
 """
-import time
 import redislite
 import redpipe
 
+# setup. configure here.
+# need to make these cli args.
+KEY_COUNT = 100
+CHUNK_SIZE = 10
+redis_client = redislite.StrictRedis()
+redpipe.connect_redis(redis_client)
 
-def bench(r, offset=0, chunk_size=10):
-    for i in range(offset, offset + chunk_size):
+
+def values_iterator():
+    for i in range(0, KEY_COUNT):
+        j = i * CHUNK_SIZE
+        values = [("%s" % v).encode('utf-8') for v in range(j, j + CHUNK_SIZE)]
+        yield values
+
+
+def bench(r, values):
+    results = []
+    for i in values:
         key = 'key%s' % i
-        r.set('key%s' % i, '1')
-        r.get(key)
+        r.set(key, i)
+        results.append(r.get(key))
+    return results
 
 
-def redispy_bench(client, iterations=100, chunk_size=10):
-    for i in range(0, iterations):
-        bench(client, offset=i * chunk_size, chunk_size=chunk_size)
-    return iterations * chunk_size
+def redispy_bench():
+    for values in values_iterator():
+        results = bench(redis_client, values)
+        assert(results == values)
 
 
-def redispipeline_bench(client, iterations=100, chunk_size=10):
-    for i in range(0, iterations):
-        with client.pipeline() as pipe:
-            bench(pipe, offset=i * chunk_size, chunk_size=chunk_size)
-            pipe.execute()
-    return iterations * chunk_size
+def redispipeline_bench():
+    for values in values_iterator():
+        with redis_client.pipeline() as pipe:
+            bench(pipe, values)
+            results = [v for i, v in enumerate(pipe.execute()) if i % 2 == 1]
+
+        assert (results == values)
 
 
-def redpipe_bench(client, iterations=100, chunk_size=10):
-    redpipe.connect_redis(client)
-
-    for i in range(0, iterations):
+def redpipe_bench():
+    for values in values_iterator():
         with redpipe.autoexec() as r:
-            bench(r, offset=i * chunk_size, chunk_size=chunk_size)
-    return iterations * chunk_size
+            results = bench(r, values)
+        assert (results == values)
 
 
-if __name__ == '__main__':
-    iterations = 2000
-    chunk_size = 20
-    redis_client = redislite.StrictRedis()
-    start = time.time()
-    redispy_bench(redis_client, iterations=iterations, chunk_size=chunk_size)
-    end = time.time()
-    elapsed = end - start
-    print("redis: %.5f" % elapsed)
+def test_redispy(benchmark):
+    redis_client.flushall()
+    benchmark(redispy_bench)
 
-    start = time.time()
-    redispipeline_bench(redis_client, iterations=iterations,
-                        chunk_size=chunk_size)
-    end = time.time()
-    elapsed = end - start
-    print("pipeline: %.5f" % elapsed)
 
-    start = time.time()
-    redpipe_bench(redis_client, iterations=iterations, chunk_size=chunk_size)
-    end = time.time()
-    elapsed = end - start
-    print("redpipe: %.5f" % elapsed)
+def test_pipeline(benchmark):
+    redis_client.flushall()
+    benchmark(redispipeline_bench)
+
+
+def test_redpipe(benchmark):
+    redis_client.flushall()
+    benchmark(redpipe_bench)
